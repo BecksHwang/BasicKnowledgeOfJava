467a7933d1303e8deb06696594ce3c50c8906a93
BecksHwang <becks920806@163.com>
2016-05-14 21:24:54 +0800

    Compare hashmap with hashtable

--- src/map/com/becks/test/HashtableTest.java
+++ src/map/com/becks/test/HashtableTest.java
@@ -42,8 +42,9 @@ public class HashtableTest {
 		System.out.println(ht);
 
 		/*
-		 * 只要两个对象通过equals比较返回true， Hashtable就认为它们是相等的value。 由于Hashtable中有一个B对象，
-		 * 它与任何对象通过equals比较都相等，所以下面输出true。
+		 * Hashtable的containsValue()方法使用e.value.equal("字符串")作比较，返回ture,
+		 * Hashtable就认为它们是相等的value。 由于Hashtable中有一个B对象，
+		 * 所以它与任何对象通过equals比较都返回ture，输出true。
 		 */
 		System.out.println(ht.containsValue("测试字符串"));
 
@@ -59,6 +60,32 @@ public class HashtableTest {
 		for (Object key : ht.keySet()) {
 			System.out.print(key + "---->");
 			System.out.print(ht.get(key) + "\n");
+		}
+
+		HashMap hm = new HashMap();
+		hm.put(new A(4), "HashMap");
+		hm.put(new A(5), "Test");
+		hm.put(new A(6), new B());
+		System.out.println(hm);
+
+		/*
+		 * HashMap的containsValue()方法使用 “字符串”.equals(e.value)作比较，返回ture,
+		 * HashMap就认为它们是相等的value。 虽然HashMap中有一个B对象， 但是返回的是false。
+		 */
+		System.out.println(hm.containsValue("测试字符串"));
+
+		// 只要两个A对象的count相等，它们通过equals比较返回true，且hashCode相等
+		// Hashtable即认为它们是相同的key，所以下面输出true。
+		System.out.println(hm.containsKey(new A(5)));
+
+		// 下面语句可以删除最后一个key-value对
+		hm.remove(new A(6));
+
+		// 通过返回Hashtable的所有key组成的Set集合，
+		// 从而遍历Hashtable每个key-value对
+		for (Object key : hm.keySet()) {
+			System.out.print(key + "---->");
+			System.out.print(hm.get(key) + "\n");
 		}
 	}
 }
