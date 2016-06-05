f8295696237f33ce9243ea48292582bdda49f057
BecksHwang <becks920806@163.com>
2016-06-05 21:58:01 +0800

    To solve the error NoClassDefFoundError

--- src/Json/com/becks/test/StringToJsonTest.java
+++ src/Json/com/becks/test/StringToJsonTest.java
@@ -6,7 +6,8 @@ import net.sf.json.JSONObject;
 public class StringToJsonTest {
 
 	public static void main(String[] args) {
-		String str = "{people:[{name:becsk,age:18},{name:hwang,age:19}]}";
+		String str = "{people:[{name:'becsk',age:18},{name:'hwang',age:19}]}";
+		System.out.println(str);
 		JSONObject jo = JSONObject.fromObject(str);
 		JSONArray ja = jo.getJSONArray("people");
 		for (int i = 0; i < ja.size(); i++) {
@@ -16,3 +17,14 @@ public class StringToJsonTest {
 	}
 
 }
+/* 报错：Exception in thread "main" java.lang.NoClassDefFoundError:
+ * 
+ * 原因：jar包引用不完全
+ * 
+ * 解决办法：引用jar包 commons-lang-2.3.jar
+ * 					ezmorp-1.0.3.jar
+ * 					commons-beanutils-1.7.0.jar
+ * 					commons-collections-3.2.1.jar
+ * 					commons-httpclient-3.1.jar
+ * 					commons-logging-1.1.1.jar
+ */
